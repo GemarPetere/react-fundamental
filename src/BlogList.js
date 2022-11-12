081a26084847dff14763462d553316a006e6719f
Gemar Petere <gemar.petereb@gmail.com>
2022-11-12 10:08:09 +0800

    first commit

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ({blogs, title, handleDelete}) =>{
+    //const blogs = props.blogs;
+
+    return(
+        <div className="blog-list">
+            <h2> { title } </h2>
+            {blogs.map((blog) => (
+                <div className='blog-previews' key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}> Hide me</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
